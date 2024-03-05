5f307c66470c6e4afc44f2640e9b7745bf3d6cc3
Hy6140 <159123191+Hy6140@users.noreply.github.com>
2024-03-05 16:50:11 +0100

    05.03.2024
    - JDT
    - Smalltests.ps1
    - Test.ps1
    - SandBox.ps1
             - Ajout de logs
             - Ajout de la fonction "WriteLog"
             - Ajout de commentaires manquants
             - Correction de la validation de quoi écrire sur le CSV
             - Adaptation du nom du .zip selon demande client

--- DevSpace/SandBox.ps1
+++ DevSpace/SandBox.ps1
@@ -1,10 +1,10 @@
 #Requires -Version 5.1
 <#
 .SYNOPSIS
-    Windows Server 2022 Parameters checker
+    WSrvChecker
  
 .DESCRIPTION
-    The script objective is to check values of specific parameters of a Windows Server 2022 and report it so that the administrator can check if a change is needed
+    The script objective is to check values of specific parameters of a Windows Server 2022 and report it to the administrator
 
 .PARAMETER ExecutionPolicy
     Bypass
@@ -36,11 +36,13 @@ param(
 $Script:ExecutionPolicy = $ExecutionPolicy
 $Script:InputFile = $InputFile
 $Script:OutputFolder = $OutputFolder
-$Script:Timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+$Script:TimeStamp = Get-Date -Format "yyyyMMdd-HHmmss"
 $Script:Data = ""
 $Script:Group = ""
 $Script:MemberToCheck = ""
 $Script:Csv = ""
+$Script:MachineName = Hostname
+$Script:LogFile = ""
 #endregion
 
 #region Modules
@@ -49,51 +51,57 @@ import-module ActiveDirectory
 
 #region Functions
 
-# Checks ExecutionPolicy
+# Write in logfile
+function WriteLog{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Private:Message
+    )
+    $Private:LogFilePath = "C:\Temp"
+    $Private:LogFileName = "Errors.log"
+    $Script:LogFile = "$Private:LogFilePath\$Private:LogFileName"
+    if (!(Test-Path $Private:LogFilePath)){
+        New-Item -ItemType Directory -Path $Private:LogFilePath
+    }
+    if(!(Test-Path $Script:LogFile)){
+        try{
+            New-Item "$Script:LogFile" -ItemType File | Out-Null
+        }catch{
+            WriteLog "Parameter: Outputfolder path is inaccessible for logfile"
+        }
+    }
+    $Private:TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $Private:LogMessage = "$Private:TimeStamp - $Private:Message"
+    Add-Content -Path "$Script:LogFile" -Value $Private:LogMessage
+}
+# Check ExecutionPolicy
 function CheckExecutionPolicy{
     # Check Executionpolicy
     if($Script:ExecutionPolicy -ne "Bypass"){
-    Write-Host "* --------" -Foreground Red
-    Write-Host "* ERROR: Script ONLY runs with the ExecutionPolicy 'Bypass'!" -Foreground Red
-    Write-Host "* Script is canceled!" -Foreground Red
-    Write-Host "* --------" -Foreground Red
-    Exit 
+        WriteLog "Script ONLY runs with the ExecutionPolicy 'Bypass'!"
+        Exit 
     }else{
-    Set-ExecutionPolicy -Scope Process -ExecutionPolicy $Script:ExecutionPolicy -Force
+        Set-ExecutionPolicy -Scope Process -ExecutionPolicy $Script:ExecutionPolicy -Force
 }
 }
-# Checks Privilieges
+# Check Privilieges
 function CheckAdminRights {
-$Private:IsAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-If (-Not $Private:IsAdmin) {
-  Write-Host "* --------" -Foreground Red
-  Write-Host "* ERROR: Script is NOT running with Administrators priviledges!" -Foreground Red
-  Write-Host "* Script is canceled!" -Foreground Red
-  Write-Host "* --------" -Foreground Red
-  Exit 
-}
+    $Private:IsAdmin = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    If (-Not $Private:IsAdmin) {
+        WriteLog "Rights: Script is NOT running with Administrators priviledges!"
+        Exit 
+    }
 }
 
+# Check if OutputFolder parameter is accessible
 function CsvOutputFile{
-    if(Test-Path $Script:OutputFolder)
+    if(!(Test-Path $Script:OutputFolder))
     {
-    try{
-        (New-Item "$Script:OutputFolder\$Script:Timestamp.csv" -ItemType File | Out-Null) -and (Test-Path$Script:OutputFolder)
-    }catch{
-            Write-Host "* --------" -Foreground Red
-            Write-Host "* ERROR: Outputfolder path is inaccessible" -Foreground Red
-            Write-Host "* Script is canceled!" -Foreground Red
-            Write-Host "* --------" -Foreground Red 
-            Exit 
+        WriteLog "Parameter: Outputfolder path is inaccessible"
+        Exit 
     }
-    }else{
-            Write-Host "* --------" -Foreground Red
-            Write-Host "* ERROR: Outputfolder path is incorrect" -Foreground Red
-            Write-Host "* Script is canceled!" -Foreground Red
-            Write-Host "* --------" -Foreground Red 
-            Exit  
-    }
-}
+        }
+
 # Check if data is ok
 function CsvInputFile {
     # Importation of CsvFile
@@ -102,14 +110,12 @@ function CsvInputFile {
         $Script:Csv = Import-CSV -Path $InputFile -Delimiter ";"
 
         }catch{
-            Write-Host "* --------" -Foreground Red
-            Write-Host "* ERROR: Import CSV file failed" -Foreground Red
-            Write-Host "* Script is canceled!" -Foreground Red
-            Write-Host "* --------" -Foreground Red
+            WriteLog "Parameter: Import CSV file failed"
             Exit
         }
     }
 
+# Check CsvInputFile data
 function CsvGetdata{
     CsvInputFile
     $Script:Data = @" 
@@ -120,20 +126,25 @@ function CsvGetdata{
         # Columns Assignment
         $Script:Group = $Private:Line.'Group'
         $Script:MemberToCheck = $Private:Line.'MemberToCheck'
-    
-            if($Private:CheckValidation = CheckUserGroupMembership){
-            $Script:Data += @"
+        $Private:CheckValidation = CheckUserGroupMembership
+        
+        # Write Data to be exported in string
+        $Private:UserExistanceChecker = UserExistanceChecker
+        $Private:GroupChecker = GroupChecker
+        if (($Private:GroupChecker) -and ($Private:UserExistanceChecker)){
+                $Script:Data += @"
 
-            $Script:Group;$Script:MemberToCheck;$Private:CheckValidation
+                $Script:Group;$Script:MemberToCheck;$Private:CheckValidation
 "@  
         }
     }
 }
 
+# 
 function UserExistanceChecker {
     try{
         if($Script:MemberToCheck -eq ""){
-        Write-Host "* Member Value is missing" -Foreground Yellow
+        WriteLog "AdCheck: Member Value is missing"
     }else{
         if($Private:UserExistanceTest = Get-ADUser -Identity $Script:MemberToCheck){
             return $true    
@@ -141,7 +152,7 @@ function UserExistanceChecker {
     }
     }catch{
         if(!($Private:UserExistanceChecker)){
-        Write-Host "* '$Script:MemberToCheck' doesn't exist or is not accessible" -Foreground Yellow
+        WriteLog "AdCheck: '$Script:MemberToCheck' doesn't exist or is not accessible"
         return $False
     }
 }
@@ -150,7 +161,7 @@ function UserExistanceChecker {
 function GroupChecker {
     try{
     if($Script:Group -eq ""){
-        Write-Host "* Group Value is missing" -Foreground Yellow
+        WriteLog "AdCheck: Group Value is missing"
     }else{
         if ($Private:GroupExistanceTest = Get-ADGroup -Identity $Script:Group){
             $Private:GroupEmptyRead = Get-ADGroup -Identity $Script:Group -Properties Members
@@ -161,10 +172,10 @@ function GroupChecker {
     }
     }catch{
         if(!($Private:GroupExistanceTest)){
-            Write-Host "* '$Script:Group' doesn't exist or is not accessible" -Foreground Yellow
+            WriteLog "AdCheck: '$Script:Group' doesn't exist or is not accessible"
             return $False
         }elseif(!($Private:GroupEmptyTest)){
-            Write-Host "* '$Script:Group' has no members" -Foreground Yellow
+            WriteLog "AdCheck: '$Script:Group' has no members"
             return $False
     }
     }
@@ -174,30 +185,23 @@ function CheckUserGroupMembership {
     # Define and calls Group and user checker ( We don't call it in the 'if' to be sure that those functions aren't executed multiple times for one data check )
     $Private:UserExistanceChecker = UserExistanceChecker
     $Private:GroupChecker = GroupChecker
+    
     if (($Private:GroupChecker) -and ($Private:UserExistanceChecker)){
         if($Private:CheckUserGroupMemberShip = Get-ADGroupMember -Identity $Script:Group | Where-Object {$_.SamAccountName -eq $Script:MemberToCheck}){
-            Write-Host "* '$Script:MemberToCheck' is in '$Script:Group'" -Foreground Green
             return $true
         }else{
-            Write-Host "* '$Script:MemberToCheck' is not in '$Script:Group'" -Foreground Cyan
             return $False
         }
     }
     if(!($Private:UserExistanceChecker)){
-        Write-Host  -Foreground Red
-        Write-Host "* ERROR: User data problem" -Foreground Red
-        Write-Host "* Script can't check line" -Foreground Red
-        Write-Host "* --------" -Foreground Red
+        WriteLog "AdCheck: User doesn't exists or is not accessible" 
     }
     if(!($Private:GroupChecker)){
-        Write-Host "* --------" -Foreground Red
-        Write-Host "* ERROR: Group data problem" -Foreground Red
-        Write-Host "* Script can't check line" -Foreground Red
-        Write-Host "* --------" -Foreground Red 
+        WriteLog "AdCheck: Group doesn't exists or is not accessible" 
     }
     }
 
-# Extract all data to a file
+# Extract all data to a .zip file
 function DataExtract{
     # Get the data
     CsvGetdata
@@ -206,16 +210,17 @@ function DataExtract{
     $Private:PSObject = $Script:Data | ConvertFrom-Csv
 
     # Export the data object to a CSV file
-    $Private:PSObject | Export-Csv -Path "$Script:OutputFolder\$Script:Timestamp.csv" -NoTypeInformation
+    $Private:PSObject | Export-Csv -Path "$Script:OutputFolder\$Script:TimeStamp.csv" -NoTypeInformation
 
     # Define the .zip file name
-    $Private:ZipFolderName = "$Script:Timestamp.zip"
+    $Private:ZipFolderName = "$($Script:MachineName)_$Script:TimeStamp.zip"
     
     # Compress .txt files to a .zip folder
-    Compress-Archive -Path "$Script:OutputFolder\$Script:Timestamp.csv" -DestinationPath "$Script:OutputFolder\$Private:ZipFolderName"
+    Compress-Archive -Path "$Script:OutputFolder\$Script:TimeStamp.csv",$Script:LogFile -DestinationPath "$Script:OutputFolder\$Private:ZipFolderName"
 
     # Delete original files
-    Remove-Item -Path "$Script:OutputFolder\$Script:Timestamp.csv"
+    Remove-Item -Path $Script:LogFile
+    Remove-Item -Path "$Script:OutputFolder\$Script:TimeStamp.csv"
     # Remove-Item -Path "$Script:InputFile"
 }
 #endregion
